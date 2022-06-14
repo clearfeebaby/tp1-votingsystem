79b3b04063d5e17cf0e80e8e89cc31938a8ca669
Mathieu G <mathieu.gonon@coding-days.com>
2022-06-14 11:55:20 +0200

    improve base code

--- contract.sol
+++ contract.sol
@@ -27,10 +27,11 @@ contract Voting is Ownable {
         VotesTallied
     }
     // Voter[] voters;
-    mapping (address => Voter) voters;
+    mapping (address => Voter) public voters;
     WorkflowStatus public workflowStatus;
-    Proposal[] proposals;
-    uint winningProposalId;
+    Proposal[] public proposals;
+    address[] whitelist = [0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2, 0x4B20993Bc481177ec7E8f571ceCaE8A9e22C02db, 0x78731D3Ca6b7E34aC0F824c42a7cC18A495cabaB, 0x617F2E2fD72FD9D5503197092aC168c91465E7f2];
+    uint public winningProposalId;
 
 
     event VoterRegistered(address voterAddress); 
@@ -38,7 +39,6 @@ contract Voting is Ownable {
     event ProposalRegistered(uint proposalId);
     event Voted (address voter, uint proposalId);
     event theWinnerIs(uint winner);
-    event showWinner(Proposal proposal);
 
     modifier onlyVoters(address _voterAddr){
         require(voters[_voterAddr].isRegistered == true, "You are not allowed to vote");
@@ -46,9 +46,15 @@ contract Voting is Ownable {
     }
 
     constructor() {
-        voters[0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2] = Voter(true, false, 0);
-        voters[0x4B20993Bc481177ec7E8f571ceCaE8A9e22C02db] = Voter(true, false, 0);
         workflowStatus = WorkflowStatus.RegisteringVoters;
+        RegisteringVoters(whitelist);
+    }
+
+    function RegisteringVoters(address[] memory _whitelist) private {
+      uint i;
+      for (i=0; i < _whitelist.length; i++) {
+        voters[_whitelist[i]] = Voter(true, false, 0);
+      }
     }
 
     function startProposalsRegistration() public onlyOwner {
@@ -71,7 +77,7 @@ contract Voting is Ownable {
         emit WorkflowStatusChange(WorkflowStatus.VotingSessionStarted, WorkflowStatus.VotingSessionEnded);
     }
 
-    function sendProposal(string memory _description) public onlyVoters(address (msg.sender)){
+    function sendProposal(string calldata _description) public onlyVoters(address (msg.sender)){
         require(workflowStatus == WorkflowStatus.ProposalsRegistrationStarted, "This isn't time for proposal.");
         proposals.push(Proposal(_description, 0));
         uint proposalId = proposals.length - 1;
@@ -86,17 +92,21 @@ contract Voting is Ownable {
         voters[msg.sender].votedProposalId = proposalId;
     }
 
-    function debugTest() public {
+    function tailledVote() public onlyOwner {
         uint i;
+        require(workflowStatus == WorkflowStatus.VotingSessionEnded, "Voting period hasn't stopped yet !");
         for (i = 0; i < proposals.length; i++) {
             if (proposals[i].voteCount > proposals[winningProposalId].voteCount) {
                 winningProposalId = i;
             }
         }
+        workflowStatus = WorkflowStatus.VotesTallied;
+        emit WorkflowStatusChange(WorkflowStatus.VotingSessionEnded, WorkflowStatus.VotesTallied);
         emit theWinnerIs(winningProposalId);
     }
 
-    function showWinnerDetails() public {
-        emit showWinner(proposals[winningProposalId]);
+    function displayWinner() public view returns (Proposal memory) {
+        require(workflowStatus == WorkflowStatus.VotingSessionEnded, "Voting period hasn't stopped yet !");
+        return proposals[winningProposalId];
     }
 }
